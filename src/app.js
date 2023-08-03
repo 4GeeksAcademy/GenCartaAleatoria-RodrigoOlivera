bd9b95838e3d2549864b17ce782981bc57d74b6a
rocady21 <68871996+rocady21@users.noreply.github.com>
2023-08-03 15:56:54 +0000

    Commit Final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,106 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+const Numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let iconsArray = ["fa-clover", "fa-spa", "fa-heart", "fa-diamond"];
+// aqui definiremos mi array para ir accediendo a el mismo
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const ArrCartas = [
+  {
+    tipo: "Corazones",
+    Numeros: Numeros,
+    icon: iconsArray[2]
+  },
+  {
+    tipo: "Pica",
+    Numeros: Numeros,
+    icon: iconsArray[1]
+  },
+  {
+    tipo: "Clubes",
+    Numeros: Numeros,
+    icon: iconsArray[0]
+  },
+  {
+    tipo: "Diamantes",
+    Numeros: Numeros,
+    icon: iconsArray[3]
+  }
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// accedemos al boton para generar una carta nueva
+const buttonGen = document.getElementById("buttonGen");
+let itemFocus = undefined;
+let itemFocusReverse = undefined;
+
+// esta variable cambiara si solo si le damos al boton de generar para saber cuando se genera con el boton y cuando no
+let varValidGen = false;
+buttonGen.addEventListener("click", GenerateCard);
+
+function GenerateCard(event) {
+  event.preventDefault();
+  console.log("xd");
+  varValidGen = true;
+  GenAleatoryCard();
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+window.onload = GenAleatoryCard;
+
+function GenAleatoryCard() {
+  // numero aleatorio para definir el array
+  const tipo = getRandomIntInclusive(0, ArrCartas.length - 1);
+  const ObjetoCarta = ArrCartas[tipo];
+  // ahora generamos el numero random para ver que carta veremos y accedemos al mismo dentro del array
+  const numRandomPalo =
+    ObjetoCarta.Numeros[
+      getRandomIntInclusive(0, ObjetoCarta.Numeros.length - 1)
+    ];
+  // obtengo los elementos para cambiarle la clase
+  const firstIcon = document.getElementById("item-icon");
+  const secondIcon = document.getElementById("item-icon-reverse");
+  const NumCard = document.getElementById("num");
+
+  if (varValidGen === true) {
+    // accedemos al padre al cual le cambiaremos el alto y ancho
+    const padre = document.getElementById("cartaPrincipal");
+    // accedo al input del ancho y alto
+    const valueAlto = document.getElementById("inputAlto").value;
+    const valueAncho = document.getElementById("inputAncho").value;
+    // cambio valores del padre
+    padre.style.height = valueAlto + "px";
+    padre.style.width = valueAncho + "px";
+
+    firstIcon.removeChild(itemFocus);
+    secondIcon.removeChild(itemFocusReverse);
+  }
+
+  // cambio el valor del texto
+  NumCard.innerHTML = numRandomPalo;
+  // creo 2 imagenes
+  // primera imagen
+  const newI = document.createElement("i");
+  newI.style.fontSize = "70px";
+  newI.style.color = "red";
+  newI.classList.add("fa-solid");
+  newI.classList.add(ArrCartas[tipo].icon);
+
+  firstIcon.appendChild(newI);
+  itemFocus = newI;
+
+  // segunda pero en reversa
+  const newIReverse = document.createElement("i");
+  newIReverse.style.fontSize = "70px";
+  newIReverse.classList.add("fa-solid");
+  newIReverse.style.color = "red";
+  newIReverse.classList.add(ArrCartas[tipo].icon);
+  newIReverse.style.transform = "rotate(-180deg)";
+
+  secondIcon.appendChild(newIReverse);
+  itemFocusReverse = newIReverse;
+}
